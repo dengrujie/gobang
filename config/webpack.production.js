136a4e455a901485db0aedc529dede302393c896
dengrujie <897623226@qq.com>
2021-02-23 09:49:57 +0800

    1,修改路径

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   output: {
-    publicPath: '/',
+    publicPath: './',
     path: join(__dirname, '../dist'),
   },
   externals: {
